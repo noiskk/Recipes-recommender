1cc7eb7f98e152a32a098672985f8c89dc7bfa7d
nois1 <zionzion00@naver.com>
2024-12-09 23:37:10 +0900

    modify user-based

--- main.py
+++ main.py
@@ -27,10 +27,31 @@ def main():
   pp_reviews_df = reviews_df.drop(columns= ['date', 'review'])
 
   # user_item_matrix = pp_reviews_df.pivot_table(index='user_id', columns='recipe_id', values='rating').fillna(0)
-  # 용량이 너무 큼. -> sparse matrix 사용
+  # 위 방식은 용량이 너무 큼. -> sparse matrix 사용
 
   sparse_user_item_matrix = csr_matrix((pp_reviews_df['rating'], 
      (pp_reviews_df['user_id'], pp_reviews_df['recipe_id'])))
+  
+  # user-based
+  def user_based_recommendations(user_id, matrix, top_k=5):
+     # 사용자 간 코사인 유사도 계산
+     user_similarity = cosine_similarity(matrix)
+     similar_users = user_similarity[user_id]  # 주어진 사용자와 다른 사용자의 유사도
+
+     # 유사도를 기준으로 정렬
+     similar_users[user_id] = 0 # 자기 자신 제외
+     similar_users_id = similar_users.argsort()[::-1] # 높은 유사도 순으로 정렬
+
+     # 비슷한 사용자가 평가한 아이템 기반 추천
+     scores = matrix[similar_users_id].toarray().sum(axis=0) # 비슷한 사용자들의 평가 합산
+     
+     recommended_items = scores.argsort()[::-1][:top_k]
+     return recommended_items
+
+  recommended_items = user_based_recommendations(0, sparse_user_item_matrix, top_k=5)
+  print("user-based : ", recommended_items)
+
+
 
 
 if __name__ == "__main__":
